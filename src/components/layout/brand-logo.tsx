4c78eeca90008d8a3768cbe34d073a7dab9c5b38
simeonpass <simeonpass@gmail.com>
2026-09-09 13:18:13 +0100

    Restore the redesign available at 07:48 BST as the launch candidate

--- src/components/layout/brand-logo.tsx
+++ src/components/layout/brand-logo.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export function BrandLogo({ onNavigate }: { onNavigate?: () => void }) {
   return (
     <Link href="/" className="ms-logo" aria-label="Mobility Station home" onClick={onNavigate}>
-      {/* Outlined SVG artwork preserves the stacked Drive & Move lettering at every size. */}
+      {/* Outlined SVG artwork preserves the approved lettering at every size. */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-light" src="/brand/drive-and-move/b-original.svg" alt="" width={920} height={488} />
+      <img className="ms-logo-light" src="/brand/refined-a/logo.svg" alt="" width={773} height={198} />
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-dark" src="/brand/drive-and-move/b-original-white.svg" alt="" width={920} height={488} />
+      <img className="ms-logo-dark" src="/brand/refined-a/logo-white.svg" alt="" width={773} height={198} />
     </Link>
   );
 }
