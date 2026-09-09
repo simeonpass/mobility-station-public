65f31aa5b40b4db2907734acd242b5ccefb187c3
simeonpass <simeonpass@gmail.com>
2026-09-09 21:05:19 +0100

    Use the live website logo in the preview header and footer

--- src/components/layout/brand-logo.tsx
+++ src/components/layout/brand-logo.tsx
@@ -3,11 +3,10 @@ import Link from "next/link";
 export function BrandLogo({ onNavigate }: { onNavigate?: () => void }) {
   return (
     <Link href="/" className="ms-logo" aria-label="Mobility Station home" onClick={onNavigate}>
-      {/* Outlined SVG artwork preserves the approved lettering at every size. */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-light" src="/brand/refined-a/logo.svg" alt="" width={773} height={198} />
+      <img className="ms-logo-light" src="/brand/mobility-station-wordmark.png" alt="" width={800} height={300} />
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-dark" src="/brand/refined-a/logo-white.svg" alt="" width={773} height={198} />
+      <img className="ms-logo-dark" src="/brand/mobility-station-wordmark-on-dark.png" alt="" width={800} height={300} />
     </Link>
   );
 }
