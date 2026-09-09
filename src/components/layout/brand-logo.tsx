4cfd3d0b30d49c11e2b58be46c41c949cf1a17bf
simeonpass <simeonpass@gmail.com>
2026-09-09 10:11:43 +0100

    Preview Drive and Move logo with wheelchair posture variations

--- src/components/layout/brand-logo.tsx
+++ src/components/layout/brand-logo.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export function BrandLogo({ onNavigate }: { onNavigate?: () => void }) {
   return (
     <Link href="/" className="ms-logo" aria-label="Mobility Station home" onClick={onNavigate}>
-      {/* Outlined SVG artwork preserves the stacked Open Road lettering at every size. */}
+      {/* Outlined SVG artwork preserves the stacked Drive & Move lettering at every size. */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-light" src="/brand/open-road-c/logo.svg" alt="" width={920} height={408} />
+      <img className="ms-logo-light" src="/brand/drive-and-move/b-original.svg" alt="" width={920} height={488} />
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-dark" src="/brand/open-road-c/logo-white.svg" alt="" width={920} height={408} />
+      <img className="ms-logo-dark" src="/brand/drive-and-move/b-original-white.svg" alt="" width={920} height={488} />
     </Link>
   );
 }
