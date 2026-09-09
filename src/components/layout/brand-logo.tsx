f1655ef26647f9dc3ba0e3ec2119746b983e87e3
simeonpass <simeonpass@gmail.com>
2026-09-09 08:29:24 +0100

    Preview stacked Open Road C logo in header and footer

--- src/components/layout/brand-logo.tsx
+++ src/components/layout/brand-logo.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export function BrandLogo({ onNavigate }: { onNavigate?: () => void }) {
   return (
     <Link href="/" className="ms-logo" aria-label="Mobility Station home" onClick={onNavigate}>
-      {/* Outlined SVG artwork preserves the approved lettering at every size. */}
+      {/* Outlined SVG artwork preserves the stacked Open Road lettering at every size. */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-light" src="/brand/refined-a/logo.svg" alt="" width={773} height={198} />
+      <img className="ms-logo-light" src="/brand/open-road-c/logo.svg" alt="" width={920} height={408} />
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className="ms-logo-dark" src="/brand/refined-a/logo-white.svg" alt="" width={773} height={198} />
+      <img className="ms-logo-dark" src="/brand/open-road-c/logo-white.svg" alt="" width={920} height={408} />
     </Link>
   );
 }
